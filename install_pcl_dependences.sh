fd6ba1b7c366cfde0274e212e80949d7ea8c52be
1368487203@qq.com <1368487203@qq.com>
2023-05-11 23:02:13 +0800

    add /e/13684/Dowloads/install_pcl_dependences.sh

---
+++ install_pcl_dependences.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get install git build-essential linux-libc-dev
+sudo apt-get install cmake cmake-gui 
+sudo apt-get install libusb-1.0-0-dev libusb-dev libudev-dev
+sudo apt-get install mpi-default-dev openmpi-bin openmpi-common
+sudo apt-get install libpcap-dev
+sudo apt-get install libflann1.9 libflann-dev
+sudo apt-get install libeigen3-dev
+sudo apt-get install libboost-all-dev
+sudo apt-get install vtk6 libvtk6.3 libvtk6-dev libvtk6.3-qt libvtk6-qt-dev 
+sudo apt-get install libqhull* libgtest-dev
+sudo apt-get install freeglut3-dev pkg-config
+sudo apt-get install libxmu-dev libxi-dev 
+sudo apt-get install mono-complete
+sudo apt-get install libopenni-dev libopenni2-dev
